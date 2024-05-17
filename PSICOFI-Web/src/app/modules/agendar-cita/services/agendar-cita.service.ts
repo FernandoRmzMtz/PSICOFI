5e721a166a326cc5c9933bfed97b38544d114450
DRKAlejandro <jesalehalo@gmail.com>
2024-05-17 15:20:59 -0600

    Mis citas

---
+++ PSICOFI-Web/src/app/modules/agendar-cita/services/agendar-cita.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AgendarCita {
+    constructor(private http: HttpClient) { }
+
+    obtenerHistorialCitas(id: number): Observable<any> {
+        const url = 'http://localhost/PSICOFI-Api/public/alumno/getRecord';
+        const body = { id: id };
+        const token = localStorage.getItem('auth_token'); 
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': token || ''
+        });
+
+        return this.http.post(url, body, { headers: headers });
+    }
+}
