4617a3de7e9409f68579883b2af6f149c906a831
Erick Padron <a325850@alumnos.uaslp.mx>
2024-05-17 13:43:56 -0600

    listado de pacientes en vista psicologo

--- PSICOFI-Web/src/app/modules/gestion-psicologos/components/tabla-alumnos-atendidos/tabla-alumnos-atendidos.component.ts
+++ PSICOFI-Web/src/app/modules/gestion-psicologos/components/tabla-alumnos-atendidos/tabla-alumnos-atendidos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { gestionPsico } from '../../services/gestion-psico.services';
 
 @Component({
   selector: 'app-tabla-alumnos-atendidos',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabla-alumnos-atendidos.component.css']
 })
 export class TablaAlumnosAtendidosComponent {
+  constructor (private gestionPsico: gestionPsico){
+    this.fetchAlumnos();
+  }
 
+  @Input() cvePsicologo: number = 0;
+
+  public alumnos: number[] = [];
+  fetchAlumnos(): void {
+    this.gestionPsico.fetchAlumnosPorPsicologo(this.gestionPsico.psicologoViendo.claveUnica ?? this.gestionPsico.psicologoViendo.curp).subscribe({
+      next: (data) => {
+        this.alumnos = Array.isArray(data) ? data : [];      },
+      error: (error) => {
+        console.error('Error fetching alumnos:', error);
+      }
+    });
+  }
 }
