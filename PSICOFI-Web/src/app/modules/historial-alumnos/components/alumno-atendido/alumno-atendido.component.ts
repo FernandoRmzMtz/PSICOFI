80f387bb67dffb950ff457ad562778ed4851dadd
Erick Padron <a325850@alumnos.uaslp.mx>
2024-05-20 22:41:58 -0600

    historial alumnos

--- PSICOFI-Web/src/app/modules/historial-alumnos/components/alumno-atendido/alumno-atendido.component.ts
+++ PSICOFI-Web/src/app/modules/historial-alumnos/components/alumno-atendido/alumno-atendido.component.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { HistorialAlumnosService } from '../../services/historial-alumnos.service';
 
+
+interface AlumnoAtendido {
+  claveUnica: number;
+  nombres: string;
+  apellidoPaterno: string;
+  apellidoMaterno: string;
+  area: string;
+  carrera: string;
+  semestre: number;
+  asesor: string;
+  condicionAcademica: string;
+  promedioGral: number;
+  creditosAprobados: number;
+  creditosInscritos: number;
+  sexo: string;
+  edad: number;
+}
+
 @Component({
   selector: 'app-alumno-atendido',
   templateUrl: './alumno-atendido.component.html',
   styleUrls: ['./alumno-atendido.component.css']
 })
+
 export class AlumnoAtendidoComponent {
+  public clave: string = '';
+  constructor(private histo: HistorialAlumnosService) {
+    this.clave = histo.alumnoViendo;
+    this.getAlumnoAtendidoInfo();
+  }
 
-  constructor(private histo: HistorialAlumnosService){}
-  
 
+  public getAlumnoAtendidoInfo(): void {
+    this.histo.getAlumnoInfo().subscribe((data) => {
+      this.histo.infoAlumno = data;
+    },
+      (error) => {
+        console.error('Error al obtener alumno:', error);
+      })
+  }
+
+  public get alumnoInfo(): AlumnoAtendido {
+    return this.histo.infoAlumno;
+  }
 }
--- PSICOFI-Web/src/app/modules/historial-alumnos/services/historial-alumnos.service.ts
+++ PSICOFI-Web/src/app/modules/historial-alumnos/services/historial-alumnos.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from "@angular/core";
+import { LoginService } from "../../login/services/login.services";
+import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { environment } from "environments/enviroment";
 
-
-
+interface AlumnoAtendido {
+  claveUnica: number;
+  nombres: string;
+  apellidoPaterno: string;
+  apellidoMaterno: string;
+  area: string;
+  carrera: string;
+  semestre: number;
+  asesor: string;
+  condicionAcademica: string;
+  promedioGral: number;
+  creditosAprobados: number;
+  creditosInscritos: number;
+  sexo: string;
+  edad: number;
+}
 
 @Injectable({
     providedIn: 'root'
@@ -11,17 +29,67 @@ export class HistorialAlumnosService {
 
   historialTablaVisible = 1;
 
-  constructor() {
+  pacientes = [];
+  alumnoViendo: string = '';
+
+  public infoAlumno: AlumnoAtendido = {
+    claveUnica: 0,
+    nombres: '',
+    apellidoPaterno: '',
+    apellidoMaterno: '',
+    area: '',
+    carrera: '',
+    semestre: 0,
+    asesor: '',
+    condicionAcademica: '',
+    promedioGral: 0,
+    creditosAprobados: 0,
+    creditosInscritos: 0,
+    sexo: '',
+    edad: 0
+  }
+
+  constructor(private loginService: LoginService, private http: HttpClient) {
     this.historialTablaVisible = 1;
   }
   public verAlumno(clave: number): void {
     this.historialTablaVisible = 0;
-    console.log(this.historialTablaVisible);
+    this.alumnoViendo = clave.toString();
   }
 
 
   public getHistorialVisible()
   {
     return this.historialTablaVisible;
+  }
+
+  public getPacientes(): Observable<any> {
+    return this.http.post(environment.api+'/psicologo/getPatients',
+      {
+        "id": this.loginService.getClave()
+      },
+      {
+        headers:
+        {
+          'Content-Type': 'application/json',
+          'X-CSRF-TOKEN': this.loginService.getToken()?? "token"
+        }
+      }
+    )
+  }
+
+  public getAlumnoInfo(): Observable<AlumnoAtendido> {
+    return this.http.post<AlumnoAtendido>(environment.api+'/alumno/getAlumno',
+      {
+        "id": this.alumnoViendo
+      },
+      {
+        headers:
+        {
+          'Content-Type': 'application/json',
+          'X-CSRF-TOKEN': this.loginService.getToken()?? "token"
+        }
+      }
+    )
   }
 }
