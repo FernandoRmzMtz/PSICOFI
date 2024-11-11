75a397d323f278cfea86fe100ef9e0ebf97a22f6
Erick Padron <erickrmirezpadron@gmail.com>
2024-11-10 22:26:45 -0600

    Bsucador de alumnos, ordenado por fecha asc y desc

--- PSICOFI-Web/src/app/modules/historial-alumnos/components/tabla-historial-citas/tabla-historial-citas.component.ts
+++ PSICOFI-Web/src/app/modules/historial-alumnos/components/tabla-historial-citas/tabla-historial-citas.component.ts
@@ -8,28 +8,55 @@ import { Router } from '@angular/router';
   styleUrls: ['./tabla-historial-citas.component.css']
 })
 export class TablaHistorialCitasComponent {
-
-  constructor(private histo: HistorialAlumnosService, private router: Router){
-    this.getHistorialCitas();
-  };
+  
   public isLoading = false;
+  public ordenAscendente: boolean = false;
+
+  constructor(private histo: HistorialAlumnosService, private router: Router) {
+    this.getHistorialCitas();
+  }
+
   public getHistorialCitas(): void {
     this.isLoading = true;
     this.histo.getHistorialCitas().subscribe((data) => {
-      this.histo.records = data;
+      this.histo.records = Array.isArray(data) ? data : [];
+
+      this.histo.records.sort((a: { fecha: string }, b: { fecha: string }) => {
+        const fechaA = new Date(a.fecha);
+        const fechaB = new Date(b.fecha);
+        return fechaA.getTime() - fechaB.getTime();
+      });
+
       this.isLoading = false;
     },
-      (error) => {
-        console.error('Error al obtener alumno:', error);
-        this.isLoading = false;
-      })
+    (error) => {
+      console.error('Error al obtener historial de citas:', error);
+      this.isLoading = false;
+    });
   }
 
   public get records() {
     return this.histo.records;
   }
 
-  redirectToReporteCitas(id:number) {
+  redirectToReporteCitas(id: number) {
     this.router.navigate(['/reporte-citas', id]);
+  }
+
+  private ordenarCitas(): void {
+    this.histo.records.sort((a: { fecha: string }, b: { fecha: string }) => {
+      const fechaA = new Date(a.fecha);
+      const fechaB = new Date(b.fecha);
+      if (this.ordenAscendente) {
+        return fechaA.getTime() - fechaB.getTime();
+      } else {
+        return fechaB.getTime() - fechaA.getTime();
+      }
+    });
+  }
+
+  public toggleOrden(): void {
+    this.ordenAscendente = !this.ordenAscendente;
+    this.ordenarCitas();
   }
 }
