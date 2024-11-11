855ac8ac2a2352d0a2ff4401b1054e92d233ee42
Erick Padron <erickrmirezpadron@gmail.com>
2024-11-10 22:52:22 -0600

    Buscador en alumnos atendidos por psicologo en administrador

--- PSICOFI-Web/src/app/modules/gestion-psicologos/components/tabla-alumnos-atendidos/tabla-alumnos-atendidos.component.ts
+++ PSICOFI-Web/src/app/modules/gestion-psicologos/components/tabla-alumnos-atendidos/tabla-alumnos-atendidos.component.ts
@@ -1,27 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { gestionPsico } from '../../services/gestion-psico.services';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabla-alumnos-atendidos',
   templateUrl: './tabla-alumnos-atendidos.component.html',
   styleUrls: ['./tabla-alumnos-atendidos.component.css']
 })
-export class TablaAlumnosAtendidosComponent {
-  constructor (private gestionPsico: gestionPsico){
+export class TablaAlumnosAtendidosComponent implements OnInit {
+  @Input() cvePsicologo: number = 0;
+
+  public alumnos: any[] = [];  
+  public alumnosFiltrados: number[] = []; 
+  public inputBuscar: string = ''; 
+
+  constructor(private gestionPsico: gestionPsico) {
+  }
+
+  ngOnInit(): void {
     this.fetchAlumnos();
   }
 
-  @Input() cvePsicologo: number = 0;
 
-  public alumnos: number[] = [];
   fetchAlumnos(): void {
     this.gestionPsico.fetchAlumnosPorPsicologo(this.gestionPsico.psicologoViendo.claveUnica ?? this.gestionPsico.psicologoViendo.curp).subscribe({
       next: (data) => {
-        this.alumnos = Array.isArray(data) ? data : [];      },
+        this.alumnos = Array.isArray(data) ? data : [];
+        this.alumnosFiltrados = [...this.alumnos]; 
+      },
       error: (error) => {
         console.error('Error fetching alumnos:', error);
       }
     });
   }
+  
+  public buscarAlumno(): void {
+    const searchQuery = this.inputBuscar.toLowerCase();  
+  
+    if (this.alumnos.length > 0) {
+      this.alumnosFiltrados = this.alumnos.filter(alumno => {
+        const clave = alumno.toString(); 
+        return clave.toLowerCase().includes(searchQuery); 
+      });
+    } else {
+      console.error('No se han encontrado alumnos.');
+    }
+  }
+  
 }
