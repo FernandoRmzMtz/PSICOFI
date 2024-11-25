9aadb5dd7ae7f46af6955b526ba1509c0d362f29
FernandoRmz32 <fernando.ramirez@catapultlabs.com>
2024-11-25 00:55:44 -0600

    removed unused imports

--- PSICOFI-Web/src/app/modules/cambio-contrasena/services/cambioContra.service.ts
+++ PSICOFI-Web/src/app/modules/cambio-contrasena/services/cambioContra.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, of } from "rxjs";
-import { LoginService } from "../../login/services/login.services";
 import { environment } from "environments/enviroment";
 import { CsrfServiceService } from "src/app/servicios/csrfService/csrf-service.service";
 
